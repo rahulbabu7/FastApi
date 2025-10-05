54228bbb46dfb8491af7826f2f76264861d234aa
rahulbabu7 <rahulbabu436@gmail.com>
2025-10-06 01:21:04 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,6 +1,10 @@
 from sqlalchemy.sql.expression import text
 from .database import Base
 from sqlalchemy import TIMESTAMP, Column,Integer,String,Boolean
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
+
 class Posts(Base):
     __tablename__ = "Posts"  #table name
 
@@ -11,6 +15,12 @@ class Posts(Base):
     published = Column(Boolean,server_default='True',nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),nullable=False,server_default=text('now()'))    #default to created now ()
     
+      # add user_id as foreign key
+    user_id = Column(Integer, ForeignKey('users.id',ondelete="CASCADE"), nullable=False)  # foreign key to Users table
+    
+    # define relationship to Users
+    user = relationship("Users", back_populates="posts")
+    
 
 class Users(Base):
     __tablename__ = 'users'
@@ -18,3 +28,5 @@ class Users(Base):
     email = Column(String,nullable=False,unique=True)
     password = Column(String,nullable=False)
     created_at = Column(TIMESTAMP(timezone=True),nullable=False,server_default=text('now()'))    #default to created now ()
+    # define relationship to Posts
+    posts = relationship("Posts", back_populates="user")
