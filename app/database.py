7cdb71de89edbb1e4b278855f6e00c711a0ea31c
rahulbabu7 <rahulbabu436@gmail.com>
2025-10-07 19:40:21 +0530

    updated the file

--- app/database.py
+++ app/database.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import sessionmaker
 # from psycopg2.extras import RealDictCursor
 # import psycopg2
 from .config import settings
+
+
 SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'  #username , password , ip addr,hostname ,dbname
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
